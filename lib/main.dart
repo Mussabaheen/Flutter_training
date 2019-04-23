cea162346b338ad3b385b66d5cdc3be369b7ff76
mussabaheen@gmail.com <mussabaheen@gmail.com>
2019-04-23 22:16:51 +0500

    button added statefull version of app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main()
 {
   runApp(new MyApp());
 }
+/*
 class MyApp extends StatelessWidget {
 
   @override
@@ -19,4 +20,77 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(title:"My App",
+      home:new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+
+
+
+class _HomePageState extends State<HomePage> {
+
+   String mytext="Hello World";
+  void _ChangeText()
+  {
+      setState(() {
+        if(mytext.startsWith('H'))
+            mytext="Welcome to my app";
+        else
+            mytext="Hello World";
+      });
+
+  }
+
+
+  Widget _bodyWidget()
+  {
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(mytext,style: new TextStyle(color: Colors.black,fontSize: 30.0),),
+            new RaisedButton(
+                child: new Text("Button",style: new TextStyle(color: Colors.white,fontSize: 20.0),),
+                onPressed: _ChangeText,
+                color: Colors.blue,
+
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+      title: new Text("Home Page"),
+
+    ),
+    body: _bodyWidget()
+    );
+  }
+}
+
 
